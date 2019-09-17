e00c1faaa9417628255781ec770382eda8abe4b1
agalvezv <43121059+agalvezv@users.noreply.github.com>
2019-09-17 19:07:39 -0400

    Part 4
    Part 4- The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,12 +8,42 @@ namespace Zork
         {
             Console.WriteLine("Welcome To Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command !=Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string response;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        response = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        response = "This is an open field west of a white house, with a boarded frnot door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        response = $"You moved {command}.";
+                        break;
+
+                    default:
+                        response = "Unknown command.";
+                        break;
+
+                }
+
+                Console.WriteLine(response);
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static Commands ToCommand(string commandString) => (Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN); //OP1 ? OP2 : OP 3
+        //Checking if true, then selects proper op
+        //=> can use if only one line within the brackets, the return is imp
     }
 }
 
