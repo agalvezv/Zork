3bbf9ff36343374ba2d5fe7a73d0bab3212fa52e
agalvezv <43121059+agalvezv@users.noreply.github.com>
2019-10-03 20:43:25 -0400

    Part 11
    Ongoing In Class Tuesday/Thursday

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -20,20 +23,12 @@ namespace Zork
             //InitializeRoomDescriptions("Rooms.txt");
             const string defaultRoomsFileName = "Rooms.txt";
             string roomsFileName = args.Length > 0 ? args[0] : defaultRoomsFileName;
-            /*
-            Equivalent of if else statement doing the same exact shit
-            */
-            //from the command line
 
             Location = IndexOf(Rooms, "West of House");
             Assert.IsTrue(Location != (-1, 1));
 
-            //start button/command line/directory(type c and d)//dotnet run   Zorkrooms.txt
-            //linq, we are querying things
             InitializeRoomDescriptions(roomsFileName);
 
-            //int a = null;      Discussion about value types, structs
-            // bool result = a == b;                 the still false example because while they have similar  data, still different obj
 
             Room previousRoom = null;
 
@@ -68,21 +63,17 @@ namespace Zork
                     case Commands.WEST:
                         if (Move(command) == false)
                         {
-                            Console.WriteLine("Area closed.");
+                            Console.WriteLine("Fuck off.");
                         }
                         break;
 
                     default:
-                        Console.WriteLine("Uknown Command.");
+                        Console.WriteLine("Uknown Command 0jnjnjnkjbhgvhgv0.");
                         break;
                 }
             }
         }
-
-        //Checking if true, then selects proper op
-        //=> can use if only one line within the brackets, the return is imp
-
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN; //THREE OPERATORS: If the first segment is true, it returns the statement before the : otherwise it returns the statement after the :.
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN; 
 
         private static readonly Commands[] Directions =
         {
@@ -159,16 +150,15 @@ namespace Zork
             return isValidMove;
         }
 
-        private static void InitializeRoomDescriptions()
+        private static void InitializeRoomDescriptions(string roomsFilename)
         {
             Dictionary<string, Room> roomMap = new Dictionary<string, Room>();
-            //Dictionary, think Websters Dictionary
-            //constant time, associative arrays,,   unique identifier
+
 
             foreach (Room room in Rooms)
             {
                 roomMap.Add(room.Name, room);
-                //roomMap[room.Name] = rooms;
+
             }
 
             string[] lines = File.ReadAllLines(roomsFilename);
@@ -183,56 +173,31 @@ namespace Zork
                     throw new InvalidDataException("Invalid record.");
                 }
 
-                string name = fields[(int)fields.Name]; //fields[0];
+                string name = fields[(int)Fields.Name]; 
                 string description = fields[(int)Fields.Description];
 
-                roomMap[name].Description = description;//fields[1];
+                roomMap[name].Description = description;
             }
 
 
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail. ";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred. ";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall. ";
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you. ";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar ";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. to the east, there appears to be sunlight. ";
-            roomMap["North House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south. ";
-
-            //modular
-            //hash tables
-            //chaining and open addressing
 
         }
+
         private enum Fields
         {
             Name = 0,
-            Dscription = 1
+            Description = 1
         }
-        //Magic numbers
+
         private enum CommandLineArguments
         {
             RoomsFilename = 0,
             UseLinq
         }
-
-
-
     }
 
 }
-
-
-//private static int LocationColumn = 1;
-// private static int LocationRow = 1;
-//private static int PlayerPosition = 1;
-////C# How To Program, C# for programmers
-
-
 
 
 
